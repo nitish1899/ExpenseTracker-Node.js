23009b76a5e2bb043d91f26101a9a2eb86f7131d
nitish1899 <nkword1899@gmail.com>
2023-01-25 23:51:33 +0530

    Leaderboard Optimisation and forget password frontend

--- controllers/premiumFeatures.js
+++ controllers/premiumFeatures.js
@@ -4,28 +4,33 @@ const User = require('../models/user');
 
 const getLeaderBoard = async (req,res) => {
     try{
-        const users = await User.findAll();
-        console.log(users);
-        const expenses = await Expense.findAll();
-        console.log(expenses);
-        const userAggregatedExpense = {};
-        expenses.forEach(expense => {
-            if(userAggregatedExpense[expense.userId]){
-                userAggregatedExpense[expense.userId] =  userAggregatedExpense[expense.userId] + expense.amount;
-            } else {
-                userAggregatedExpense[expense.userId] = expense.amount; 
-            }
+        const users = await User.findAll({
+            attributes : ['id', 'name']
         });
+        //console.log(users);
+        const userAggregatedExpense = await Expense.findAll({
+            attributes : [ [sequelize.fn('sum',sequelize.col('amount')),'userId']],
+            group: ['userId']
+        });
+        //console.log(userAggregatedExpense);
+       //const userAggregatedExpense = {};
+        // expenses.forEach(expense => {
+        //     if(userAggregatedExpense[expense.userId]){
+        //         userAggregatedExpense[expense.userId] =  userAggregatedExpense[expense.userId] + expense.amount;
+        //     } else {
+        //         userAggregatedExpense[expense.userId] = expense.amount; 
+        //     }
+        // });
        
         const userLeaderBoard = [];
         users.forEach(user => {
             userLeaderBoard.push( {name: user.name, amount : userAggregatedExpense[user.id] || 0});
         });
         userLeaderBoard.sort((a , b) => b.amount - a.amount);
-        console.log(userLeaderBoard);
+        //console.log(userLeaderBoard);
 
         console.log(userAggregatedExpense);
-        res.status(200).json(userLeaderBoard);
+        res.status(200).json(userAggregatedExpense);
     } catch(err) {
        console.log(err);
        res.status(500).json(err);
